0b0e7f6ade93ecaa9515e3f46f1c3a0df0dd43e1
MuhammadAbsarSiddiqui <128141992+MuhammadAbsarSiddiqui@users.noreply.github.com>
2024-01-19 23:43:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+//ASSIGNMENT 1
+
+//***Q1***//
+
+var age = 20;
+var isStudent = true;
+console.log('*******Q1******');
+console.log('My age is', age);
+console.log('I am a student:', isStudent);
+
+
+
+
+//***Q2***//
+
+alert("***WELCOME***    welcome to my website here i have completed my assignment 1 of websites")
+
+
